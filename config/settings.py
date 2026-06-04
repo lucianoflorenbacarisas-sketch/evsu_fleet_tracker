7c02453c2697deff31fff1c49e864c6e550a5143
lucianoflorenbacarisas-sketch <lucianofloren.bacarisas@evsu.edu.ph>
2026-06-04 09:40:35 -0700

    Allow Render host in ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-f@)6j9)3h#kie-zhvbqef
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,evsu-fleet-tracker.onrender.com'
+).split(',')
 
 
 # Application definition
