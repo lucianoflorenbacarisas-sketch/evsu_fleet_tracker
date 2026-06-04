fce39d63a3fc73bf73b9373877e4722cdd7a9824
lucianoflorenbacarisas-sketch <lucianofloren.bacarisas@evsu.edu.ph>
2026-06-04 09:08:20 -0700

    Track logs app and remove logs/ from .gitignore

---
+++ logs/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from accounts.models import User
+
+
+class ActivityLog(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    action = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+
+        return f"{self.user.username} - {self.action}"
